cc691a099d2221638d169a3478f2d14bb6c06d95
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-07-23 08:48:21 -0300

    deleting unnecessary component and editing main file

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Microphones can be used not only to pick up sound, but also to project sound similar to a speaker.</h1>
+
+  <input type="radio" name="options" value="true">
+  <label>True</label><br>
+
+  <input type="radio" name="options" value="false">
+  <label>False</label><br>
+
+  <button type="button" class="send">Send</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -21,6 +25,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
